2fe6b91a8824f9fc3141aa6fcc6c92c712186e36
sadat <shameemsadat00@gmail.com>
2024-04-03 23:17:14 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/material_to_do/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.material_to_do
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:material_to_do/homepage.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
